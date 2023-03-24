a29a16deff762185296848ab4f7e659884cd1b3b
Brett Larney <93brett@gmail.com>
2023-03-24 14:46:28 -0400

    initialize

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+   return (
+         <View>
+            <Text>test</Text>
+         </View>
+   )
+}
+export default App;
